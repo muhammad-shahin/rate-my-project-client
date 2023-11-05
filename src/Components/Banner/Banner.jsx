76e9d61706e5d2095c1464d8399cdb6500918b90
muhammad-shahin <muhammadshahin002@gmail.com>
2023-11-05 09:09:48 +0600

    particle bg added for banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,12 +2,17 @@ import GlassButton from '../../ReuseableUI/GlassButton/GlassButton';
 import OutlineButton from '../../ReuseableUI/OutlineButton/OutlineButton';
 import Lottie from 'lottie-react';
 import bannerAnim from '../../assets/Animation/banner-animation.json';
+import ParticlesBackground from '../ParticlesBackground/ParticlesBackground';
 const Banner = () => {
   return (
-    <section className='rounded-lg xl:px-[8%] xl:py-20 px-[5%] py-10 bg-primary backdrop-blur-xl bg-opacity-[0.75] flex-bet'>
+    <section className='xl:px-[8%] xl:py-20 px-[5%] py-10 bg-primary backdrop-blur-xl bg-opacity-[0.75] flex-bet relative'>
+      {/* particles */}
+      <div className='absolute top-0 left-0'>
+        <ParticlesBackground />
+      </div>
       {/* left side */}
       <div className='capitalize text-white space-y-6'>
-        <h1 className='font-semibold text-2xl w-full lg:text-6xl lg:w-3xl'>
+        <h1 className='font-semibold text-2xl w-full lg:text-6xl lg:max-w-3xl'>
           Unlock Your Academic Potential: Transform Your Study Experience with
           Collaborative Learning
         </h1>
