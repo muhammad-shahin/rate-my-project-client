247f9034083323f377daa61bb1c51c1c5f0a97c2
muhammad-shahin <muhammadshahin002@gmail.com>
2023-11-05 11:31:57 +0600

    links updated for nav items

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,13 +6,14 @@ import ParticlesBackground from '../ParticlesBackground/ParticlesBackground';
 const Banner = () => {
   return (
     <section className='xl:px-[8%] xl:py-20 px-[5%] py-10 backdrop-blur-xl bg-opacity-[0.75] flex-bet relative gradient-bg'>
+      {/* overlay */}
       {/* particles */}
       <div className='absolute top-0 left-0'>
         <ParticlesBackground />
       </div>
       {/* left side */}
       <div className='capitalize text-white space-y-6'>
-        <h1 className='font-semibold text-2xl w-full lg:text-6xl lg:max-w-3xl'>
+        <h1 className='font-semibold text-2xl w-full lg:text-6xl lg:max-w-3xl gradient-text '>
           Unlock Your Academic Potential: Transform Your Study Experience with
           Collaborative Learning
         </h1>
@@ -34,6 +35,7 @@ const Banner = () => {
           animationData={bannerAnim}
         />
       </div>
+      <div className='overlay'></div>
     </section>
   );
 };
--- src/Layouts/Navbar/Navbar.jsx
+++ src/Layouts/Navbar/Navbar.jsx
@@ -56,10 +56,10 @@ const Navbar = () => {
         <ul
           className={` xl:static fixed top-[75px] ${
             isOpen ? 'right-0' : 'right-[-100%]'
-          } xl:h-auto h-screen xl:w-auto w-[50%] flex xl:flex-row flex-col bg-gray-200 dark:bg-[#00aaff6c] backdrop-blur-[25px] xl:px-5 py-2 justify-start xl:justify-center items-center xl:gap-10 gap-8 duration-700 z-[100] xl:rounded-full xl:pt-2 pt-10`}
+          } xl:h-auto h-screen xl:w-auto w-[50%] flex xl:flex-row flex-col bg-gray-200 dark:bg-[#00aaff6c] backdrop-blur-[25px] xl:px-5 py-2 justify-start xl:justify-center items-center duration-700 z-[100] xl:rounded-full xl:pt-2 pt-10`}
         >
           <li
-            className='text-black dark:text-black font-medium text-[16px] hover:scale-[1.1] duration-500'
+            className='text-black dark:text-white font-medium text-[16px] nav-item grad-bg duration-500 hover:text-white rounded-full px-5 py-2'
             onClick={() => {
               setOpen(false);
             }}
@@ -67,34 +67,34 @@ const Navbar = () => {
             <NavLink to='/'>Home</NavLink>
           </li>
           <li
-            className='text-black dark:text-black font-medium text-[16px] hover:scale-[1.1] duration-500'
+            className='text-black dark:text-white font-medium text-[16px] nav-item grad-bg duration-500 hover:text-white rounded-full px-5 py-2'
             onClick={() => {
               setOpen(false);
             }}
           >
-            <NavLink to='/myCart'>My Cart</NavLink>
+            <NavLink to='/my-assignment'>My Assignment</NavLink>
           </li>
           <li
-            className='text-black dark:text-black font-medium text-[16px] hover:scale-[1.1] duration-500'
+            className='text-black dark:text-white font-medium text-[16px] nav-item grad-bg duration-500 hover:text-white rounded-full px-5 py-2'
             onClick={() => {
               setOpen(false);
             }}
           >
-            <NavLink to='/brand/adidas'>Shop</NavLink>
+            <NavLink to='/all-assignment'>All Assignment</NavLink>
           </li>
           <li
-            className='text-black dark:text-black font-medium text-[16px] hover:scale-[1.1] duration-500'
+            className='text-black dark:text-white font-medium text-[16px] nav-item grad-bg duration-500 hover:text-white rounded-full px-5 py-2'
             onClick={() => {
               setOpen(false);
             }}
           >
-            <NavLink to='/addProduct'>Add Product</NavLink>
+            <NavLink to='/create-assignment'>Create Assignment</NavLink>
           </li>
           {user ? (
             ''
           ) : (
             <li
-              className='text-black dark:text-black font-medium text-[16px] hover:scale-[1.1] duration-500'
+              className='text-black dark:text-white font-medium text-[16px] nav-item grad-bg duration-500 hover:text-white rounded-full px-5 py-2'
               onClick={() => {
                 setOpen(false);
               }}
@@ -106,7 +106,7 @@ const Navbar = () => {
         {/* cart icons */}
         <div className='flex justify-center items-center gap-5'>
           <DarkTheme />
-          <div className='bg-gray-200 opacity-[0.7] backdrop-blur-lg rounded-full p-2 cursor-pointer hover:opacity-[1] duration-500 hover:bg-primary hover:bg-opacity-[0.39] hover:text-white relative  dark:opacity-[1] dark:text-primary'>
+          <div className='bg-gray-200 opacity-[0.7] backdrop-blur-lg rounded-full p-2 cursor-pointer hover:opacity-[1] hover:bg-primary hover:bg-opacity-[0.39] hover:text-white relative  dark:opacity-[1] dark:text-primary'>
             <AiOutlineBell
               onClick={() => {
                 navigate('/myCart');
